5de61aa752734decb0cad3b182cd00ce45f07763
Biswajit Nayak <biswajit678@gmail.com>
2025-10-29 20:35:00 +0530

    Profile setup

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -67,3 +67,11 @@ export const login = async (req, res) => {
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 };
+export const logout = async (req,res)=>{
+    try {
+        res.status(200).json("Loggedout Successfully")
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
